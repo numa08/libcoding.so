b9431629495f6acc0c4dbd238fb5923b2286c496
numa08 <n511287@gmail.com>
2014-01-01 21:12:21 +0900

    オサレ

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/libcoding/"
+http_images_path = "/libcoding/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/libcoding/fonts"
+css_dir = "public/libcoding/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
---
+++ source/what-libcoding/index.markdown
@@ -0,0 +1,20 @@
+---
+layout: page
+title: "Live Coding de Night"
+date: 2014-01-01 20:55
+comments: true
+sharing: true
+footer: true
+---
+
+
+Coding、それはプログラマの呼吸である。
+
+新鮮な情報を取り入れ、世界を構築するコードを吐き出す。Live Coding de Nightは、そんな僕達の息吹をお互いに目で、耳で、そして肌で感じ合う、プログラマによるエンタテインメント。
+
+
+## What??
+
+Live Coding de Nightはライブコーディングのイベントだ。普段仕事で、趣味でコードを書いているプログラマがノリの良い音楽とともにコードを書くパフォーマンスを行う。
+
+最高に盛り上がる夜を共に。
